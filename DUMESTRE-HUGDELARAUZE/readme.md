57c27b63edb14ba8b0cbbd8d883dcb544c00cf25
Sebastien HUG DE LARAUZE <sebastien.hugdelarauze@protonmail.ch>
2019-09-07 18:23:44 +0200

    Initial commit

---
+++ DUMESTRE-HUGDELARAUZE/readme.md
@@ -0,0 +1,12 @@
+# ReadMe
+
+TODO: Description
+
+## Project folder overview
+
+TODO
+
+## Authors
+
+* DUMESTRE Lucas
+* HUG DE LARAUZE Sébastien
---
+++ DUMESTRE-HUGDELARAUZE/src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * HelloWorld program
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        if (args.length > 1) {
+            String output = "Hello " + String.join(" ", args);
+            System.out.println(output);
+        } else {
+            System.out.println("Hello World !");
+        }
+    }
+}
