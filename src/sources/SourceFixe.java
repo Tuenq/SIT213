ed2595f1611a59e6defeea5146fcfc98f48fffa5
mathieu-83 <mathieu.bartoli@imt-atlantique.net>
2019-09-16 15:51:32 +0200

    Code de la classe source fixe

--- src/sources/SourceFixe.java
+++ src/sources/SourceFixe.java
@@ -1,9 +1,26 @@
 package sources;
 
+import information.InformationNonConforme;
+
 /**
  * SourceFixe
  */
 public class SourceFixe extends Source<Boolean> {
 
-    
+    public SourceFixe(){
+        super();
+    }
+
+    public void emettre(String messagefixe) throws InformationNonConforme {
+        for(int i=0;i<messagefixe.length();i++) {
+            if(messagefixe.charAt(i) == '1')
+                informationGeneree.add(true);
+            else if(messagefixe.charAt(i) == '0')
+                informationGeneree.add(false);
+            else {
+                throw new InformationNonConforme("un ou plusieurs caractères ne sont pas de type booleen");
+            }
+        }
+        super.emettre();
+    }
 }
