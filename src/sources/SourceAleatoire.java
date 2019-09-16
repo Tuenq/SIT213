6abbacd7790c0595e66c7787cf2a3602429fe881
Ludovic <lebordelaisdu31@gmail.com>
2019-09-16 17:36:58 +0200

    v1 source aléatoire

--- src/sources/SourceAleatoire.java
+++ src/sources/SourceAleatoire.java
@@ -1,9 +1,44 @@
 package sources;
 
+import information.Information;
+
+import java.lang.Boolean;
+import java.util.*;
+
+
+
 /**
  * SourceAleatoire
  */
 public class SourceAleatoire extends Source<Boolean> {
 
-    
+    int taille;
+
+    public SourceAleatoire(int nbBit) {
+        taille = nbBit;
+
+        for (int j=0; j<= taille; j++) {
+            informationGeneree = new Information<Boolean>();
+            Random rnd = new Random();
+            boolean value = rnd.nextBoolean();
+            informationGeneree.add(value);
+        }
+    }
+
+    public SourceAleatoire(int nbBit, long seed) {
+        taille = nbBit;
+
+        for (int j = 0; j <= taille; j++) {
+            informationGeneree = new Information<Boolean>();
+            Random rnd = new Random(seed);
+            boolean value = rnd.nextBoolean();
+            informationGeneree.add(value);
+        }
+
+    }
+
+    public static String toString(){
+        System.out.println();
+    }
 }
+
