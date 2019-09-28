6025094d55c1ac7f0437c675e110ce0160c939be
mathieu-83 <mathieu.bartoli@imt-atlantique.net>
2019-09-28 17:03:11 +0200

    Création d'une classe de test

---
+++ test/Step3.java
@@ -0,0 +1,28 @@
+
+import org.junit.Test;
+
+
+public class Step3 {
+    @Test
+    public void visualisation_NRZT() throws Exception {
+        String[] args = {"-s", "-seed", "123", "-nbEch", "1000", "-form", "NRZT"};
+        Simulateur simulateur = new Simulateur(args);
+        simulateur.execute();
+    }
+
+    @Test
+    public void visualisation_NRZ() throws Exception {
+        String[] args = {"-s", "-seed", "123", "-nbEch", "1000", "-form", "NRZ"};
+        Simulateur simulateur = new Simulateur(args);
+        simulateur.execute();
+    }
+
+    @Test
+    public void visualisation_RZ() throws Exception {
+        String[] args = {"-s", "-seed", "123", "-nbEch", "1000", "-form", "RZ"};
+        Simulateur simulateur = new Simulateur(args);
+        simulateur.execute();
+    }
+}
+
+
