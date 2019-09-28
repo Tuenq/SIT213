b46585ef5496d979218871cffa7d4cb11187f93e
mathieu-83 <mathieu.bartoli@imt-atlantique.net>
2019-09-28 17:10:08 +0200

    Ajout des tests avec un SNR

--- test/Step3.java
+++ test/Step3.java
@@ -5,21 +5,21 @@ import org.junit.Test;
 public class Step3 {
     @Test
     public void visualisation_NRZT() throws Exception {
-        String[] args = {"-s", "-seed", "123", "-nbEch", "1000", "-form", "NRZT"};
+        String[] args = {"-s", "-seed", "-snr", "10", "123", "-nbEch", "1000", "-form", "NRZT"};
         Simulateur simulateur = new Simulateur(args);
         simulateur.execute();
     }
 
     @Test
     public void visualisation_NRZ() throws Exception {
-        String[] args = {"-s", "-seed", "123", "-nbEch", "1000", "-form", "NRZ"};
+        String[] args = {"-s", "-seed", "-snr", "10", "123", "-nbEch", "1000", "-form", "NRZ"};
         Simulateur simulateur = new Simulateur(args);
         simulateur.execute();
     }
 
     @Test
     public void visualisation_RZ() throws Exception {
-        String[] args = {"-s", "-seed", "123", "-nbEch", "1000", "-form", "RZ"};
+        String[] args = {"-s", "-seed", "-snr", "10", "123", "-nbEch", "1000", "-form", "RZ"};
         Simulateur simulateur = new Simulateur(args);
         simulateur.execute();
     }
