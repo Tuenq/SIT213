9824fb2cedcd5d14430d74ae76d19df29408c6ff
mathieu-83 <mathieu.bartoli@imt-atlantique.net>
2019-09-28 17:21:06 +0200

    Preparation en attente du calcul de la Gaussienne en fonction du SNR

--- test/Step3.java
+++ test/Step3.java
@@ -5,23 +5,33 @@ import org.junit.Test;
 public class Step3 {
     @Test
     public void visualisation_NRZT() throws Exception {
-        String[] args = {"-s", "-seed", "-snr", "10", "123", "-nbEch", "1000", "-form", "NRZT"};
+        String[] args = {"-s", "-seed", "123", "-snr", "10", "-nbEch", "1000", "-form", "NRZT"};
         Simulateur simulateur = new Simulateur(args);
         simulateur.execute();
     }
 
     @Test
     public void visualisation_NRZ() throws Exception {
-        String[] args = {"-s", "-seed", "-snr", "10", "123", "-nbEch", "1000", "-form", "NRZ"};
+        String[] args = {"-s", "-seed", "123", "-snr", "10", "-nbEch", "1000", "-form", "NRZ"};
         Simulateur simulateur = new Simulateur(args);
         simulateur.execute();
     }
 
     @Test
     public void visualisation_RZ() throws Exception {
-        String[] args = {"-s", "-seed", "-snr", "10", "123", "-nbEch", "1000", "-form", "RZ"};
+        String[] args = {"-s", "-seed", "123", "-snr", "10", "-nbEch", "1000", "-form", "RZ"};
         Simulateur simulateur = new Simulateur(args);
         simulateur.execute();
+    }
+
+    @Test
+    public void visualisation_TEB() throws Exception {
+        for(int i=0;i<100;i++){
+            String[] args = {"-s", "-mess", "00000000000", "-snr", "0", "-nbEch", "1000", "-form", "NRZ"};
+            Simulateur simulateur = new Simulateur(args);
+            
+        }
+
     }
 }
 
