b0deb3c40a3faee66e06ff8a313ea61ddffc0671
mathieu-83 <mathieu.bartoli@imt-atlantique.net>
2019-09-28 17:22:32 +0200

    != SRN pour chaque forme (permet de tester à la fois la forme et le bruit)

--- test/Step3.java
+++ test/Step3.java
@@ -12,14 +12,14 @@ public class Step3 {
 
     @Test
     public void visualisation_NRZ() throws Exception {
-        String[] args = {"-s", "-seed", "123", "-snr", "10", "-nbEch", "1000", "-form", "NRZ"};
+        String[] args = {"-s", "-seed", "123", "-snr", "40", "-nbEch", "1000", "-form", "NRZ"};
         Simulateur simulateur = new Simulateur(args);
         simulateur.execute();
     }
 
     @Test
     public void visualisation_RZ() throws Exception {
-        String[] args = {"-s", "-seed", "123", "-snr", "10", "-nbEch", "1000", "-form", "RZ"};
+        String[] args = {"-s", "-seed", "123", "-snr", "100", "-nbEch", "1000", "-form", "RZ"};
         Simulateur simulateur = new Simulateur(args);
         simulateur.execute();
     }
@@ -29,7 +29,7 @@ public class Step3 {
         for(int i=0;i<100;i++){
             String[] args = {"-s", "-mess", "00000000000", "-snr", "0", "-nbEch", "1000", "-form", "NRZ"};
             Simulateur simulateur = new Simulateur(args);
-            
+
         }
 
     }
