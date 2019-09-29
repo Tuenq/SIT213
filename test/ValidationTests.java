97e4cff4cab3b2bbe6cb7af1ab6599747f1d1767
Lucas Dumestre <lucas.dumestre@wanadoo.fr>
2019-09-29 14:54:02 +0200

    Méthode de tests de validations ajouté
    Pour étape 1,2 et 3 la génération des valeurs du TEB est possible manque la récupération de ce dernier pour genérer la courbe

---
+++ test/ValidationTests.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class ValidationTests {
+
+    private static void validationEtape1() {
+        String[] args1 = {"-s", "-seed", "123"};
+        String[] args2 = {"-s", "-mess", "000000111111"};
+
+        try {
+            Simulateur simulateur = new Simulateur(args1);
+            simulateur.execute();
+
+            simulateur = new Simulateur(args2);
+            simulateur.execute();
+        }
+        catch (Exception exception){
+            exception.printStackTrace();
+            System.out.println("Erreur durant le test de validation etape 1");
+        }
+    }
+
+    private static void validationEtape2() {
+        String[] args1 = {"-s", "-seed", "123", "-nbEch", "1000", "-form", "RZ"};
+        String[] args2 = {"-s", "-seed", "123", "-nbEch", "1000", "-form", "NRZ"};
+        String[] args3 = {"-s", "-seed", "123", "-nbEch", "1000", "-form", "NRZT"};
+        try {
+            Simulateur simulateur = new Simulateur(args1);
+            simulateur.execute();
+
+            simulateur = new Simulateur(args2);
+            simulateur.execute();
+
+            simulateur = new Simulateur(args3);
+            simulateur.execute();
+        }
+        catch (Exception exception){
+            System.out.println("Erreur durant le test de validation etape 2");
+        }
+    }
+
+    private static void validationEtape3() {
+        String[] args = {"-s", "-seed", "123", "-nbEch", "1000", "-form", "RZ"};
+        String[] args2;
+        try {
+            Simulateur simulateur = new Simulateur(args);
+            simulateur.execute();
+
+            int snr;
+            for(int i = 0 ; i < 1000000 ; i++){
+                snr = 10 - (10/1000000) * i;
+                args2 = new String[]{"-mess", "100000", "-snr", Integer.toString(snr), "-nbEch", "1000", "-form", "NRZ"};
+                simulateur = new Simulateur(args2);
+                simulateur.execute();
+            }
+        }
+        catch (Exception exception){
+            System.out.println("Erreur durant le test de validation etape 3 affichage signal bruité");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Pour quelle étape voulez vous lancer un test de validation ?");
+
+        Scanner scanner = new Scanner(System.in);
+
+        switch (scanner.nextLine()){
+            case "1":
+                validationEtape1();
+                break;
+            case "2":
+                validationEtape2();
+                break;
+            case "3":
+                validationEtape3();
+                break;
+            default:
+                System.out.println("Etape non reconue");
+        }
+
+    }
+}
